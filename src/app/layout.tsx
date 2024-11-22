8de3b664fb99d73e50668dac955170ecdd6fdaf8
Keven Ages <keven@duhado.com>
2024-11-22 10:58:08 -0500

    Added toast and toastprovider and used it where error handling makes sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@
 import './globals.css';
 import ClientWrapper from '../components/ClientWrapper';
 import { TooltipProvider } from '../components/ui/tooltip';
+import ToastProvider from '../components/ToastProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">
         <TooltipProvider>
+          <ToastProvider />
           <ClientWrapper>{children}</ClientWrapper>
         </TooltipProvider>
       </body>
